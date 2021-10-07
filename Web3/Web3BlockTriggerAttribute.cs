1d5d87d93ed220a012394b7055ba5babac83fea0
Yushi Sato <yushi.kinger@gmail.com>
2021-10-07 22:00:19 +0900

    first commit

---
+++ Web3/Web3BlockTriggerAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.WebJobs.Description;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebJobs.Extensions.Web3.BlockTrigger.Web3
+{
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    [Binding]
+    public class Web3BlockTriggerAttribute: Attribute
+    {
+        public string Endpoint { get; set; }
+        public int FromHeight { get; set; }
+        public int Confirmation { get; set; }
+
+        public Web3BlockTriggerAttribute(string endpoint, int fromHeight, int confirmation)
+        {
+            Endpoint = endpoint;
+            FromHeight = fromHeight;
+            Confirmation = confirmation;
+        }
+
+        public Web3BlockTriggerAttribute(string endpoint, string fromHeight, int confirmation)
+        {
+            Endpoint = endpoint;
+            if (int.TryParse(fromHeight, out int from))
+            {
+                FromHeight = from;
+            }
+            else
+            {
+                FromHeight = -1;
+            }
+            Confirmation = confirmation;
+        }
+
+        public Web3BlockTriggerAttribute(string endpoint) : this(endpoint, -1, 12) { }
+        public Web3BlockTriggerAttribute(string endpoint, string fromHeight) : this(endpoint, fromHeight, 12) { }
+        public Web3BlockTriggerAttribute(string endpoint, int confirmation) : this(endpoint, "latest", confirmation) { }
+    }
+}
---
+++ Web3BlockTriggerStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using System;
+using WebJobs.Extensions.Web3.BlockTrigger;
+
+[assembly: WebJobsStartup(typeof(Web3BlockTriggerStartup), "Timers, Files and Warmup")]
+namespace WebJobs.Extensions.Web3.BlockTrigger
+{
+    public class Web3BlockTriggerStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.
+        }
+    }
+}

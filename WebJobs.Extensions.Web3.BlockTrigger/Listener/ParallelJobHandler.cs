47ef8a508fc55db58a0af5edf4698b0242415242
Yushi Sato <yushi.kinger@gmail.com>
2021-10-10 02:49:40 +0900

    parallel job handler added. stil unused.

---
+++ WebJobs.Extensions.Web3.BlockTrigger/Listener/ParallelJobHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebJobs.Extensions.Web3.BlockTrigger.Listener
+{
+    public static class ParallelJobHandler
+    {
+        public static async Task<T> RunParallelAndAwaitFirstCompletion<T>(IEnumerable<Task<T>> tasks)
+        {
+            using(var cts = new CancellationTokenSource())
+            {
+                var tasksWithCts = tasks.Select(x => Task<T>.Factory.StartNew(() => x.Result, cts.Token));
+                var firstCompletedTask = await Task.WhenAny(tasksWithCts);
+                var res = await firstCompletedTask;
+                cts.Cancel();
+                return res;
+            }
+        }
+    }
+}
